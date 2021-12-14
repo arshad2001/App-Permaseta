2cbd8195f99563cf50469da5ba2e3fae80be7ffe
arshad2001 <62998839+arshad2001@users.noreply.github.com>
2021-12-14 19:52:55 +0700

    initial commit

---
+++ lib/home.dart
@@ -0,0 +1,150 @@
+import 'package:flutter/material.dart';
+import 'package:nani/theme.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: AppColor.background,
+          elevation: 0.0,
+        ),
+        body: Container(
+          color: AppColor.background,
+          child: ListView(
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.only(left: 35, right: 35, bottom: 20),
+                child: 
+                Container(
+                  height: 40,
+                  decoration: BoxDecoration(
+                      color: AppColor.paleYellow,
+                      shape: BoxShape.rectangle,
+                      borderRadius: BorderRadius.all(Radius.circular(50.0)),
+                    ),
+                    child: Padding(
+                      padding: EdgeInsets.only(left: 20, right: 20, top: 5, bottom: 5,),                  
+                      child: Row(
+                        children: [
+                          Padding(padding: EdgeInsets.only(right: 70,),
+                            child:
+                              Container(
+                                height: 25,
+                                width: 25,
+                                color: Colors.black,
+                              ), 
+                          ),
+                          Text('search for events'),
+                        ],
+                      ),
+                    ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(left: 35, right: 35, bottom: 15),
+                child: 
+                Container(
+                  height: 155,
+                  decoration: BoxDecoration(
+                    color: AppColor.paleYellow,
+                    shape: BoxShape.rectangle,
+                    borderRadius: BorderRadius.all(Radius.circular(15.0)),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(left: 35, right: 35, bottom: 15),
+                child: 
+                Container(
+                  height: 155,
+                  decoration: BoxDecoration(
+                    color: AppColor.paleYellow,
+                    shape: BoxShape.rectangle,
+                    borderRadius: BorderRadius.all(Radius.circular(15.0)),
+                  ),
+                  child: Text('Nice')
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(left: 35, right: 35, bottom: 15),
+                child: 
+                Container(
+                  height: 155,
+                  decoration: BoxDecoration(
+                    color: AppColor.paleYellow,
+                    shape: BoxShape.rectangle,
+                    borderRadius: BorderRadius.all(Radius.circular(15.0)),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(left: 35, right: 35, bottom: 15),
+                child: 
+                Container(
+                  height: 155,
+                  decoration: BoxDecoration(
+                    color: AppColor.paleYellow,
+                    shape: BoxShape.rectangle,
+                    borderRadius: BorderRadius.all(Radius.circular(15.0)),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+        drawer: 
+        Padding(
+          padding: EdgeInsets.only(top: 30),
+          child: 
+          Container(
+          width: 300.0,
+          height: 810.0,
+          decoration: BoxDecoration(
+            color: AppColor.background,
+            shape: BoxShape.rectangle,
+            borderRadius: BorderRadius.only(
+              topRight: Radius.circular(25.0),
+              bottomRight: Radius.circular(25.0)),
+          ),
+          child: ListView(
+          // Important: Remove any padding from the ListView.
+          padding: EdgeInsets.zero,
+          children: [
+            Container(
+              margin: const EdgeInsets.all(30.0),
+              height: 235,
+              decoration: BoxDecoration(
+                color: AppColor.paleYellow,
+                shape: BoxShape.rectangle,
+                borderRadius: BorderRadius.all(Radius.circular(20)),
+              ),
+              child: Container(
+                margin: const EdgeInsets.all(15.0),
+                decoration: BoxDecoration(
+                color: AppColor.palishYellow,
+                  shape: BoxShape.rectangle,
+                  borderRadius: BorderRadius.all(Radius.circular(15)),
+                ),
+              ),
+            ),
+            ListTile(
+              title: const Text('Item 1'),
+              onTap: () {
+                // Update the state of the app.
+                // ...
+              },
+            ),
+            ListTile(
+              title: const Text('Item 2'),
+              onTap: () {
+                // Update the state of the app.
+                // ...
+              },
+            ),
+          ],
+        ),)
+      ),
+    );
+  }
+}
---
+++ lib/theme.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  AppColor._();
+  static const Color back = Color(0xffFEF5ED);
+  static const Color lightGreen = Color(0xffD3E4CD);
+  static const Color armyGreen = Color(0xffADC2A9);
+  static const Color white = Color(0xffFFFFFF);
+  static const Color paleGreen = Color(0xffEFFFEA);
+  static const Color neonGreen = Color(0xffD3E4CD);
+
+  static const Color background = Color(0xffFFDC40);
+  static const Color paleYellow = Color(0xffFFF1C0);
+  static const Color palishYellow = Color(0xffFADD63);
+
+}
