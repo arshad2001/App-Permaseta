0b89a77a2d56f25a4e85b7ed7c9833b55938f91f
Calseto <72954173+Calseto@users.noreply.github.com>
2022-01-18 10:27:59 +0700

    update 18-01-2022

--- lib/home.dart
+++ lib/home.dart
@@ -5,146 +5,145 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: AppColor.background,
-          elevation: 0.0,
-        ),
-        body: Container(
-          color: AppColor.armyGreen,
-          child: ListView(
-            children: <Widget>[
-              Padding(
-                padding: EdgeInsets.only(left: 35, right: 35, bottom: 20),
-                child: 
-                Container(
+      appBar: AppBar(
+        backgroundColor: AppColor.background,
+        elevation: 0.0,
+      ),
+      body: Container(
+        color: AppColor.background,
+        child: ListView(
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.only(left: 35, right: 35, bottom: 20),
+              child: Container(
                   height: 40,
                   decoration: BoxDecoration(
-                      color: AppColor.paleYellow,
-                      shape: BoxShape.rectangle,
-                      borderRadius: BorderRadius.all(Radius.circular(50.0)),
-                    ),
-                    child: Padding(
-                      padding: EdgeInsets.only(left: 20, right: 20, top: 5, bottom: 5,),                  
-                      child: Row(
-                        children: [
-                          Padding(padding: EdgeInsets.only(right: 70,),
-                            child:
-                              Container(
-                                height: 25,
-                                width: 25,
-                                color: Colors.black,
-                              ), 
-                          ),
-                          Text('search for events'),
-                        ],
+                    color: AppColor.paleYellow,
+                    shape: BoxShape.rectangle,
+                    borderRadius: BorderRadius.all(Radius.circular(50.0)),
+                  ),
+                  child: Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(left: 10.0),
+                        child: Icon(
+                          Icons.search,
+                          size: 32.0,
+                        ),
                       ),
-                    ),
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.only(left: 35, right: 35, bottom: 15),
-                child: 
-                Container(
-                  height: 155,
-                  decoration: BoxDecoration(
-                    color: AppColor.paleYellow,
-                    shape: BoxShape.rectangle,
-                    borderRadius: BorderRadius.all(Radius.circular(15.0)),
-                  ),
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.only(left: 35, right: 35, bottom: 15),
-                child: 
-                Container(
-                  height: 155,
-                  decoration: BoxDecoration(
-                    color: AppColor.paleYellow,
-                    shape: BoxShape.rectangle,
-                    borderRadius: BorderRadius.all(Radius.circular(15.0)),
-                  ),
-                  child: Text('Nice')
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.only(left: 35, right: 35, bottom: 15),
-                child: 
-                Container(
-                  height: 155,
-                  decoration: BoxDecoration(
-                    color: AppColor.paleYellow,
-                    shape: BoxShape.rectangle,
-                    borderRadius: BorderRadius.all(Radius.circular(15.0)),
-                  ),
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.only(left: 35, right: 35, bottom: 15),
-                child: 
-                Container(
-                  height: 155,
-                  decoration: BoxDecoration(
-                    color: AppColor.paleYellow,
-                    shape: BoxShape.rectangle,
-                    borderRadius: BorderRadius.all(Radius.circular(15.0)),
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ),
-        drawer: 
-        Padding(
-          padding: EdgeInsets.only(top: 30),
-          child: 
-          Container(
-          width: 300.0,
-          height: 810.0,
-          decoration: BoxDecoration(
-            color: AppColor.background,
-            shape: BoxShape.rectangle,
-            borderRadius: BorderRadius.only(
-              topRight: Radius.circular(25.0),
-              bottomRight: Radius.circular(25.0)),
-          ),
-          child: ListView(
-          // Important: Remove any padding from the ListView.
-          padding: EdgeInsets.zero,
-          children: [
-            Container(
-              margin: const EdgeInsets.all(30.0),
-              height: 235,
-              decoration: BoxDecoration(
-                color: AppColor.paleYellow,
-                shape: BoxShape.rectangle,
-                borderRadius: BorderRadius.all(Radius.circular(20)),
-              ),
+                      Expanded(
+                        child: Padding(
+                          padding: const EdgeInsets.only(right: 42.0),
+                          child: TextField(
+                            decoration: InputDecoration(
+                                hintText: "Search", border: InputBorder.none),
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                      )
+                    ],
+                  )),
+            ),
+            Padding(
+              padding: EdgeInsets.only(left: 35, right: 35, bottom: 15),
               child: Container(
-                margin: const EdgeInsets.all(15.0),
+                height: 155,
                 decoration: BoxDecoration(
-                color: AppColor.palishYellow,
+                  image: DecorationImage(
+                      image: AssetImage("Assets/sandwave.png"),
+                      fit: BoxFit.cover),
+                  color: AppColor.paleYellow,
                   shape: BoxShape.rectangle,
-                  borderRadius: BorderRadius.all(Radius.circular(15)),
+                  borderRadius: BorderRadius.all(Radius.circular(15.0)),
                 ),
               ),
             ),
-            ListTile(
-              title: const Text('Item 10'),
-              onTap: () {
-                // Update the state of the app.
-                // ...
-              },
+            Padding(
+              padding: EdgeInsets.only(left: 35, right: 35, bottom: 15),
+              child: Container(
+                  height: 155,
+                  decoration: BoxDecoration(
+                    color: AppColor.paleYellow,
+                    shape: BoxShape.rectangle,
+                    borderRadius: BorderRadius.all(Radius.circular(15.0)),
+                  ),
+                  child: Text('Nice')),
             ),
-            ListTile(
-              title: const Text('Item 2'),
-              onTap: () {
-                // Update the state of the app.
-                // ...
-              },
+            Padding(
+              padding: EdgeInsets.only(left: 35, right: 35, bottom: 15),
+              child: Container(
+                height: 155,
+                decoration: BoxDecoration(
+                  color: AppColor.paleYellow,
+                  shape: BoxShape.rectangle,
+                  borderRadius: BorderRadius.all(Radius.circular(15.0)),
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(left: 35, right: 35, bottom: 15),
+              child: Container(
+                height: 155,
+                decoration: BoxDecoration(
+                  color: AppColor.paleYellow,
+                  shape: BoxShape.rectangle,
+                  borderRadius: BorderRadius.all(Radius.circular(15.0)),
+                ),
+              ),
             ),
           ],
-        ),)
+        ),
       ),
+      drawer: Padding(
+          padding: EdgeInsets.only(top: 30),
+          child: Container(
+            width: 300.0,
+            height: 810.0,
+            decoration: BoxDecoration(
+              color: AppColor.background,
+              shape: BoxShape.rectangle,
+              borderRadius: BorderRadius.only(
+                  topRight: Radius.circular(25.0),
+                  bottomRight: Radius.circular(25.0)),
+            ),
+            child: ListView(
+              // Important: Remove any padding from the ListView.
+              padding: EdgeInsets.zero,
+              children: [
+                Container(
+                  margin: const EdgeInsets.all(30.0),
+                  height: 235,
+                  decoration: BoxDecoration(
+                    color: AppColor.paleYellow,
+                    shape: BoxShape.rectangle,
+                    borderRadius: BorderRadius.all(Radius.circular(20)),
+                  ),
+                  child: Container(
+                    margin: const EdgeInsets.all(15.0),
+                    decoration: BoxDecoration(
+                      color: AppColor.palishYellow,
+                      shape: BoxShape.rectangle,
+                      borderRadius: BorderRadius.all(Radius.circular(15)),
+                    ),
+                  ),
+                ),
+                ListTile(
+                  title: const Text('Item 10'),
+                  onTap: () {
+                    // Update the state of the app.
+                    // ...
+                  },
+                ),
+                ListTile(
+                  title: const Text('Item 2'),
+                  onTap: () {
+                    // Update the state of the app.
+                    // ...
+                  },
+                ),
+              ],
+            ),
+          )),
     );
   }
 }
